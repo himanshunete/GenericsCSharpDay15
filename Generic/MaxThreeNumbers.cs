b666fc8248b59687154cc45add44f0718668f94f
himanshunete <himanshuneteh@gmail.com>
2021-02-15 07:31:00 +0530

    [himanshunete]Add FindMaximumFloatValue

--- Generic/MaxThreeNumbers.cs
+++ Generic/MaxThreeNumbers.cs
@@ -33,5 +33,29 @@ namespace Generic
             }
             return secondNumber;
         }
+
+        /// <summary>
+        /// UC-2 To find maximum float value
+        /// </summary>
+        /// <param name="firstNumber"></param>
+        /// <param name="secondNumber"></param>
+        /// <param name="thirdNumber"></param>
+        /// <returns></returns>
+        public float MaxOfThreeFloats(float firstNumber, float secondNumber, float thirdNumber)
+        {
+            Console.WriteLine("Maximum Number using non generic for float value");
+            if (firstNumber.CompareTo(secondNumber) > 0 && secondNumber.CompareTo(thirdNumber) > 0)
+                return firstNumber;
+
+            else if (secondNumber.CompareTo(firstNumber) > 0 && secondNumber.CompareTo(thirdNumber) > 0)
+            {
+                return secondNumber;
+            }
+            else if (thirdNumber.CompareTo(firstNumber) > 0 && thirdNumber.CompareTo(secondNumber) > 0)
+            {
+                return thirdNumber;
+            }
+            return firstNumber;
+        }
     }
 }
--- NUnitTestProject/UnitTest1.cs
+++ NUnitTestProject/UnitTest1.cs
@@ -17,7 +17,7 @@ namespace NUnitTestProject
         /// TC-1.1 Test max at 1st position
         /// </summary>
         [Test]
-        public void Given3Numbers_WhenAt1stPosition_ShouldReturnMaxValue()
+        public void Given3IntegerNumbers_WhenAt1stPosition_ShouldReturnMaxValue()
         {
             int result = maxThreeNumbers.MaxOfThreeNumbers(89, 23, 65);
             Assert.AreEqual(89, result);
@@ -27,7 +27,7 @@ namespace NUnitTestProject
         /// TC-1.2 Test max at 2nd position
         /// </summary>
         [Test]
-        public void Given3Numbers_WhenAt2ndPosition_ShouldReturnMaxValue()
+        public void Given3IntegerNumbers_WhenAt2ndPosition_ShouldReturnMaxValue()
         {
             int result = maxThreeNumbers.MaxOfThreeNumbers(23, 89, 65);
             Assert.AreEqual(89, result);
@@ -37,10 +37,40 @@ namespace NUnitTestProject
         /// TC-1.3 Test max at 3rd position
         /// </summary>
         [Test]
-        public void Given3Numbers_WhenAt3rdPosition_ShouldReturnMaxValue()
+        public void Given3IntegerNumbers_WhenAt3rdPosition_ShouldReturnMaxValue()
         {
             int result = maxThreeNumbers.MaxOfThreeNumbers(23, 65, 89);
             Assert.AreEqual(89, result);
+        }
+
+        /// <summary>
+        /// TC-2.1 Test max at 1st position
+        /// </summary>
+        [Test]
+        public void Given3FloatNumbers_WhenAt1stPosition_ShouldReturnMaxValue()
+        {
+            float result = maxThreeNumbers.MaxOfThreeFloats(22.03f, 13.98f, 6.34f);
+            Assert.AreEqual(22.03f, result);
+        }
+
+        /// <summary>
+        /// TC-2.2 Test max at 2nd position
+        /// </summary>
+        [Test]
+        public void Given3FloatNumbers_WhenAt2ndPosition_ShouldReturnMaxValue()
+        {
+            float result = maxThreeNumbers.MaxOfThreeFloats(13.98f, 22.08f, 6.34f);
+            Assert.AreEqual(22.03f, result);
+        }
+
+        /// <summary>
+        /// TC-2.3 Test max at 3rd position
+        /// </summary>
+        [Test]
+        public void Given3FloatNumbers_WhenAt3rdPosition_ShouldReturnMaxValue()
+        {
+            float result = maxThreeNumbers.MaxOfThreeFloats(13.98f, 6.34f, 22.08f);
+            Assert.AreEqual(22.03f, result);
         }
     }
 }
